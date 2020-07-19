862cc6190fba0920884695c2a29aba8a1017886b
rishikant42 <rshkntshrm@gmail.com>
2020-07-19 19:38:43 +0530

    added element-ui

--- frontend/eshop/src/main.js
+++ frontend/eshop/src/main.js
@@ -4,11 +4,14 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
+Vue.use(ElementUI);
 
 new Vue({
   router,
