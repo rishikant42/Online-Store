bae389da9941aa51bf72c7aeca4a53cb9a9c43d8
rishikant42 <rshkntshrm@gmail.com>
2020-07-20 21:01:05 +0530

    removed useless code

--- frontend/eshop/src/router/index.js
+++ frontend/eshop/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 import Products from "../views/Products.vue";
+import Home from "../views/Products.vue";
 
 Vue.use(VueRouter);
 
@@ -9,16 +9,8 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    redirect: "/products",
     component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
     path: "/products",
