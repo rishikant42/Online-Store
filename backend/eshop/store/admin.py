0c13514556af2065e5e5713c560ea94cc451e563
rishikant42 <rshkntshrm@gmail.com>
2020-07-21 08:46:30 +0530

    added admin interface

--- backend/eshop/store/admin.py
+++ backend/eshop/store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import (
+    Category, SubCategory, Product,
+)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Product, ProductAdmin)
